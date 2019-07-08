f75777e3be89bb27db5425407c27a7294b9b383c
Makavura <makavuramughanga@gmail.com>
2019-07-08 08:39:26 +0300

    get array from firestore db via subscribe

--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
-import { ItemsService } from '../shared/items.service';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { ItemsService, Item } from '../shared/items.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -9,20 +9,44 @@ import { Observable } from 'rxjs';
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css']
 })
+
+
 export class ItemListComponent implements OnInit {
+
   items: Observable<any[]>;
+  itemTable: any;
+  lessons  = []
+
   constructor(db: AngularFirestore, private itemService: ItemsService) {
     this.items = db.collection('items').valueChanges();
 
-  // constructor(private itemService: ItemsService) { }
+  //   db.collection('items').get().then( z => querySnapshot => {
+  //     querySnapshot.forEach(y => doc => {
+  //         // doc.data() is never undefined for query doc snapshots
+  //         console.log(doc.id, " => ", doc.data());
+  //     });
+  // });
   }
+
   ngOnInit() {
    this.itemService.getItems();
+  //  this.items.subscribe(val => (console.log(this.itemService.anan()))
+   this.items.subscribe(lessons => this.lessons = lessons)
+   console.log(this.lessons);
+   return this.lessons;
+
+
+ 
+  //  console.log()
   }
 
-  itemis;
 
-  getItems = () => this.itemService.getItems().subscribe(res => (this.itemis = res));
+
+  getItems() {
+  this.itemService.getItems();
+  }
 
 
 }
+
+
