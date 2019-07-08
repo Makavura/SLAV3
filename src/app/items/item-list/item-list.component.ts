3d4729b111ad87c9b4eca522017bd071ce2eaa16
Makavura <makavuramughanga@gmail.com>
2019-07-08 14:02:00 +0300

    add functionality to compute sum of prices and total number items

--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -14,7 +14,8 @@ import { Observable } from 'rxjs';
 export class ItemListComponent implements OnInit {
 
   items: Observable<any[]>;
-
+  itemis: Item[];
+  ittems: any;
 
   constructor(db: AngularFirestore, private itemService: ItemsService) {
     this.items = db.collection('items').valueChanges();
@@ -22,12 +23,37 @@ export class ItemListComponent implements OnInit {
   }
 
   ngOnInit() {
+
    this.itemService.getItems();
-   this.items.subscribe(val => console.log(val));
-   // returns an array
+  //  this.items.subscribe(val => console.log(val));
+   this.items.subscribe(data => {
+    // console.log(data);
+    this.itemis = data.map(e => {
+      // console.log(e, data, data.length);
+      // for (dat in data ) {
+      //   console.log(dat)
+      // }
+      // let i =0;
+      // while(i < data.length){
+        var titems: number[] = []
+      for(var t= 0; t<data.length; t++ ) {
+         
+         titems.push(data[t].price)
+        }
+        var total = titems.reduce(function(a,b){return a+b})
+        console.log(titems, total, titems.length)
+      // }
+      return {
+        id: e.payload.doc.id,
+        data,
+        ...e
+      } as Item;
+    });
+  });
+
+  // const totalPrice =
+
   }
-
-
 
   getItems() {
   this.itemService.getItems();
